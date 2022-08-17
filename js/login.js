ca3b650d2ee6f68177a4fb6ed30bfd9a63dc2b5b
Rafin Hossain <rafin8990@gmail.com>
2022-08-17 16:16:49 +0600

    code commited

--- js/login.js
+++ js/login.js
@@ -3,7 +3,7 @@ document.getElementById('btn-submit').addEventListener('click', function(){
     const email=emailField.value 
     const passwordField=document.getElementById('user-password')
     const password=passwordField.value 
-    if(email==='sontan@baap.com'&& password==='secret'){
+    if(email==='panda@hood.com'&& password==='secret'){
         window.location.href='bank.html';
     }
     else{
